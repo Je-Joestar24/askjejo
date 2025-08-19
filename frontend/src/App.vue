1417542bbd118e2eee2c2cfd5d2b069d573ce9bb
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-19 20:44:20 +0800

    feat: Created the CSS for footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,13 +3,22 @@ import { RouterView } from 'vue-router'
 import {onMounted} from 'vue'
 import api from '@/config/api'
 import Navigation from './components/header/Navigation.vue';
+import Footer from './components/footer/footer.vue';
 
 const callApi =  async () => {
   try {
+        // Use await to wait for the API call to complete
         const response = await api.post('/test');
+
+        // Access the response data from the server
         console.log('Success! Data from server:', response.data);
+
+        // You can now update your application's state or display a success message
     } catch (error) {
+        // Handle any errors that occurred during the request
         console.error('There was an error with the API call:', error.response.data);
+
+        // You can display an error message to the user here
     }
 }
 onMounted(callApi)
@@ -18,4 +27,5 @@ onMounted(callApi)
 <template>
   <Navigation/>
   <RouterView />
+  <Footer />
 </template>
