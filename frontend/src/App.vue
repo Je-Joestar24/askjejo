06a97daaf04c4f76319ef4c2e7442abfe8d7c917
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-19 17:41:16 +0800

    ref: Created the Html Structure for Navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,27 +2,20 @@
 import { RouterView } from 'vue-router'
 import {onMounted} from 'vue'
 import api from '@/config/api'
+import Navigation from './components/header/Navigation.vue';
 
 const callApi =  async () => {
   try {
-        // Use await to wait for the API call to complete
         const response = await api.post('/test');
-
-        // Access the response data from the server
         console.log('Success! Data from server:', response.data);
-
-        // You can now update your application's state or display a success message
     } catch (error) {
-        // Handle any errors that occurred during the request
         console.error('There was an error with the API call:', error.response.data);
-
-        // You can display an error message to the user here
     }
 }
 onMounted(callApi)
 </script>
 
 <template>
-
+  <Navigation/>
   <RouterView />
 </template>
