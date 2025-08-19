a1a004f0befc694155bb5b829cb4e2b3a2bf544c
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-19 21:04:56 +0800

    ref: Cleaned up the Navs made it dynamic

--- frontend/src/components/footer/Navs.vue
+++ frontend/src/components/footer/Navs.vue
@@ -1,11 +1,28 @@
 <template>
     <!-- Center nav with glow -->
     <nav class="footer__nav" aria-label="Footer navigation">
-        <div class="footer__nav-glow" aria-hidden="true"></div>
-        <ul class="footer__links" role="list">
-            <li class="footer__item"><a href="/" class="footer__link">Home</a></li>
-            <li class="footer__item"><a href="/about" class="footer__link">About</a></li>
-            <li class="footer__item"><a href="/ask" class="footer__link">Ask</a></li>
-        </ul>
+      <div class="footer__nav-glow" aria-hidden="true"></div>
+      <ul class="footer__links" role="list">
+        <li 
+          v-for="item in navItems" 
+          :key="item.to" 
+          class="footer__item"
+        >
+          <RouterLink :to="item.to" class="footer__link">
+            {{ item.label }}
+          </RouterLink>
+        </li>
+      </ul>
     </nav>
-</template>
+  </template>
+  
+  <script setup>
+  import { RouterLink } from 'vue-router'
+  
+  const navItems = [
+    { label: 'Home', to: '/' },
+    { label: 'About', to: '/about' },
+    { label: 'Ask', to: '/ask' },
+  ]
+  </script>
+  
