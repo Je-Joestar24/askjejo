008ddade7c63e30e54ee68803e2bc67512b5e249
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-20 01:37:20 +0800

    feat: Ceated the Modal contents and its CSS

---
+++ frontend/src/components/modals/login/LoginForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="result" class="loginmodal__message" :class="result?.success ? 'success' : 'error'" aria-live="polite"
+        @click.prevent="result = null">
+        {{ result?.message }}
+    </div>
+    <form class="loginmodal__form" autocomplete="off" @submit.prevent="alert()">
+        <div class="loginmodal__field">
+            <label for="login-email" class="loginmodal__label">Email</label>
+            <input id="login-email" type="email" v-model="login_form.email" class="loginmodal__input"
+                placeholder="Enter your email" required autocomplete="username" />
+        </div>
+        <div class="loginmodal__field">
+            <label for="login-password" class="loginmodal__label">Password</label>
+            <input id="login-password" type="password" v-model="login_form.password" class="loginmodal__input"
+                placeholder="Enter your password" required autocomplete="current-password" />
+        </div>
+        <button type="submit" class="loginmodal__button" aria-label="Login">Login</button>
+    </form>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const login_form = ref({ email: '', password: '' })
+</script>
---
+++ frontend/src/components/modals/login/LoginModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="loginmodal__backdrop" role="dialog" aria-modal="true" aria-label="Login Modal" @click="toggleModal()">
+    <div class="loginmodal__modal" tabindex="0" @click.stop>
+      <button class="loginmodal__close" aria-label="Close login modal" @click.prevent="toggleModal()">&times;</button>
+      <div class="loginmodal__header">
+        <h2 class="loginmodal__title">Login</h2>
+      </div>
+      <LoginForm />
+      <div class="loginmodal__footer">
+        <span class="loginmodal__signup-text">Don't have an account?</span>
+        <a href="#" @click.prevent="toggleModal('signup')" class="loginmodal__signup-link" aria-label="Sign up">Sign
+          up</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { useStore } from 'vuex'
+import LoginForm from './LoginForm.vue'
+
+const store = useStore()
+
+const toggleModal = (active = '') => {
+  store.dispatch('setActiveModal', active)
+}
+</script>
