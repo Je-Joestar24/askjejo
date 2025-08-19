6f304f230606306b822c41101a65e4e1ebb6c842
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-19 13:37:13 +0800

    ref: Created subcomponents for Mission and Vision CSS

--- frontend/src/components/about/Intro.vue
+++ frontend/src/components/about/Intro.vue
@@ -8,6 +8,4 @@
 <script setup>
 import Decorations from './Intro/Decorations.vue';
 import MainContents from './Intro/MainContents.vue';
-
-// UI-only component; styles live in assets/about/intro/index.css
 </script>
