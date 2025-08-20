e78f9a27f406a6372b71fbf6d7cc479e3025d72e
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-21 02:05:52 +0800

    ref: Profile Components Mounted

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,8 +1,9 @@
 <template>
-    
+    <MainDisplay/>
 </template>
 
 <script setup>
+import MainDisplay from '@/components/profile/MainDisplay.vue';
 import { onMounted } from 'vue';
 
 onMounted(() => document.title = "AskJejo | Profile")
