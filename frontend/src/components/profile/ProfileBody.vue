157012ee8b4cc8616c524177abe593e2fd685896
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-21 18:47:09 +0800

    ref: Created the ChangePassoword component for Profile Form

--- frontend/src/components/profile/ProfileBody.vue
+++ frontend/src/components/profile/ProfileBody.vue
@@ -2,34 +2,7 @@
     <form @submit.prevent="" class="profile-form">
         <Email />
         <Username />
-        <!-- Change Password Section -->
-        <div class="password-section" v-show="state.isEditing">
-            <button @click="togglePasswordChange" type="button" class="change-password-btn"
-                :class="{ 'active': state.showPasswordChange }" aria-label="Change password">
-                <svg class="lock-icon" viewBox="0 0 24 24" fill="currentColor">
-                    <path
-                        d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10z" />
-                </svg>
-                {{ state.showPasswordChange ? 'Cancel Password Change' : 'Change Password' }}
-            </button>
-
-            <!-- Password Change Form -->
-            <div v-if="state.showPasswordChange" class="password-form">
-                <div class="form-group">
-                    <label for="currentPassword" class="form-label">Current Password</label>
-                    <input id="currentPassword" type="password" class="form-input" placeholder="Enter current password"
-                        aria-describedby="current-password-help" />
-                    <div id="current-password-help" class="form-help">Enter your current password</div>
-                </div>
-
-                <div class="form-group">
-                    <label for="newPassword" class="form-label">New Password</label>
-                    <input id="newPassword" type="password" class="form-input" placeholder="Enter new password"
-                        aria-describedby="new-password-help" />
-                    <div id="new-password-help" class="form-help">Enter your new password</div>
-                </div>
-            </div>
-        </div>
+        <ChangePassword/>
         <div class="form-actions" v-if="state.isEditing">
             <button type="button" @click="" class="btn btn-secondary" aria-label="Cancel changes">
                 Cancel
@@ -49,6 +22,7 @@
 import { useStore } from 'vuex'
 import Email from './Email.vue'
 import Username from './username.vue'
+import ChangePassword from './ChangePassword.vue'
 
 const store = useStore()
 const state = store.state.profile
