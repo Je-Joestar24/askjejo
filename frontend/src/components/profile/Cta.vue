f28e886918c7b88bb32a63f79a53d120f3ff52e5
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-21 18:48:33 +0800

    ref: Created the CTA component for Profile Form

---
+++ frontend/src/components/profile/Cta.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="form-actions" v-if="state.isEditing">
+        <button type="button" @click="" class="btn btn-secondary" aria-label="Cancel changes">
+            Cancel
+        </button>
+        <button type="submit" class="btn btn-primary" :disabled="!false" aria-label="Save profile changes">
+            <svg class="save-icon" viewBox="0 0 24 24" fill="currentColor">
+                <path
+                    d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z" />
+            </svg>
+            Save Changes
+        </button>
+    </div>
+</template>
+
+<script setup>
+import { useStore } from 'vuex'
+
+const store = useStore()
+const state = store.state.profile
+</script>
--- frontend/src/components/profile/ProfileBody.vue
+++ frontend/src/components/profile/ProfileBody.vue
@@ -2,19 +2,8 @@
     <form @submit.prevent="" class="profile-form">
         <Email />
         <Username />
-        <ChangePassword/>
-        <div class="form-actions" v-if="state.isEditing">
-            <button type="button" @click="" class="btn btn-secondary" aria-label="Cancel changes">
-                Cancel
-            </button>
-            <button type="submit" class="btn btn-primary" :disabled="!false" aria-label="Save profile changes">
-                <svg class="save-icon" viewBox="0 0 24 24" fill="currentColor">
-                    <path
-                        d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z" />
-                </svg>
-                Save Changes
-            </button>
-        </div>
+        <ChangePassword />
+        <Cta />
     </form>
 </template>
 
@@ -23,6 +12,7 @@ import { useStore } from 'vuex'
 import Email from './Email.vue'
 import Username from './username.vue'
 import ChangePassword from './ChangePassword.vue'
+import Cta from './Cta.vue'
 
 const store = useStore()
 const state = store.state.profile
