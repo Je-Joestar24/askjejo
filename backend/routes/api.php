4335b013e6f9a3738e3d8311087d34e60ee7d3e0
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-22 16:17:31 +0800

    ref: Fixed the signup

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\api\UserAuths;
 
 Route::post('/auth/signup', [UserAuths::class, 'signup']);
 
+Route::get('/users', [UserAuths::class, 'index']);
+
 Route::post('/test', function () {
     return response()->json([
         'laravel_version' => app()->version(),
