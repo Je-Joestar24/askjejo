b3362976127d042ff009f3a358fd8daccd96279e
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-27 12:53:57 +0800

    ref: Created the route message and mounted on the App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,6 +7,7 @@ import Modals from './components/modals/Modals.vue';
 import Loading from './components/loading/Loading.vue';
 import { onMounted } from 'vue';
 import Notification from './components/notif/Notification.vue';
+import RouteMessage from './components/routemessage/RouteMessage.vue';
 
 const store = useStore()
 const state = store.state
@@ -19,5 +20,6 @@ onMounted(() => { store.dispatch('initialize') })
   <FooterV />
   <Modals />
   <Loading :show="state.loading" />
-  <Notification/>
+  <Notification />
+  <RouteMessage />
 </template>
---
+++ frontend/src/components/routemessage/RouteMessage.vue
@@ -0,0 +1,3 @@
+<template>
+    Rerouted
+</template>
