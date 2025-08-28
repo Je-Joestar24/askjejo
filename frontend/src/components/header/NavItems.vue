bb373b912579f91bf71d55d0269870a3d81997c1
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-28 20:31:50 +0800

    ref: Updated the navigation to be displayed on an ask page

--- frontend/src/components/header/NavItems.vue
+++ frontend/src/components/header/NavItems.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- Navigation links -->
     <ul class="header__nav-list" role="list">
-        <li v-for="item in navItems" :key="item.to" class="header__nav-item">
+        <li v-if="route.name != 'ask'" v-for="item in filteredNavItems" :key="item.to" class="header__nav-item">
             <RouterLink :to="item.to" class="header__nav-link" active-class="header__nav-link--active">
                 {{ item.label }}
             </RouterLink>
@@ -10,10 +10,25 @@
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router'
+import { computed, ref } from 'vue';
+import { RouterLink, useRoute } from 'vue-router'
+import { useStore } from 'vuex';
 
-const navItems = [
+const store = useStore()
+const route = useRoute();
+
+const navItems = ref([
     { label: 'Home', to: '/' },
     { label: 'About', to: '/about' },
-]
+    { label: 'Ask', to: '/ask' }
+])
+
+
+const filteredNavItems = computed(() => {
+    if (store.state.user.logged_user) {
+        return navItems.value.filter(item => !['/', '/about'].includes(item.to))
+    } else {
+        return navItems.value.filter(item => ['/', '/about'].includes(item.to))
+    }
+})
 </script>
--- frontend/src/components/header/Navigation.vue
+++ frontend/src/components/header/Navigation.vue
@@ -1,12 +1,16 @@
 <template>
-  <header class="header" role="banner" aria-label="Main navigation">
-    <Accent/>
-    <NavContents/>
+  <header class="header" role="banner" :style="route.name == 'ask' ? 'border-bottom: none' : ''"
+    aria-label="Main navigation">
+    <Accent />
+    <NavContents />
   </header>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router';
 import Accent from './Accent.vue';
 import NavContents from './NavContents.vue';
 // Component logic will be added later
+
+const route = useRoute()
 </script>
