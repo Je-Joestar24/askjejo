28b0086ff98e347241cb065a5a30e8fdd162bccf
Jejomar Parrilla <jpar1252003@gmail.com>
2025-08-28 16:28:54 +0800

    ref: Created the CSS styles for the Ask page

--- frontend/src/views/AskView.vue
+++ frontend/src/views/AskView.vue
@@ -1,9 +1,10 @@
 <template>
-    
+    <Ask />
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
+import Ask from '@/components/ask/Ask.vue'
 
 onMounted(() => document.title = "AskJejo | Ask")
 </script>
