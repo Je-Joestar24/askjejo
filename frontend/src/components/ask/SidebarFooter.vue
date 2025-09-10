624cfc65bd65de564016171a299926d586c70035
Jejomar Parrilla <jpar1252003@gmail.com>
2025-09-10 21:27:34 +0800

    Frontend: feat(ask): Sidebar Footer actions created

--- frontend/src/components/ask/SidebarFooter.vue
+++ frontend/src/components/ask/SidebarFooter.vue
@@ -1,6 +1,24 @@
 <template>
     <footer class="ask__sidebar-footer" aria-label="Sidebar actions">
-        <button class="ask__sidebar-btn" aria-label="Settings">Profile</button>
-        <button class="ask__sidebar-btn" aria-label="Logout">Logout</button>
+        <RouterLink to="/profile" class="ask__sidebar-btn" style="text-align: center;" aria-label="Settings">Profile</RouterLink>
+        <button class="ask__sidebar-btn" aria-label="Logout" @click.prevent="logoutUser">Logout</button>
     </footer>
 </template>
+
+<script setup>
+import { useRouter, RouterLink } from 'vue-router'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const router = useRouter()
+
+const logoutUser = async () => {
+    const response = await store.dispatch('logout')
+    if (response.success) {
+        store.dispatch('initialize')
+        store.commit('notif/setMessage', { message: response.message, type: 'success' })
+        router.push({ name: 'home' })
+    } else store.commit('notif/setMessage', { message: response.message, type: 'error' })
+
+}
+</script>
